85b759ea15bfec4b50618071d4932c588f85255e
JaeGao <jg6523@nyu.edu>
2024-05-07 05:05:42 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,15 +11,15 @@ let playerSize = 30;
 let coinSize = 20;
 let lavaY = 600;
 let scrollSpeed = 1;
-let startPlatform;
 let playerVelocity = 0;
 const jumpForce = -12;
 const gravity = 0.6;
 let playerWidth = 154;
 let playerHeight = 365;
+let firstJump = false;
 function preload() {
   player = loadImage('assets/player.png');
-  coin = loadImage('assets/coin.png');
+  coinImage = loadImage('assets/coin.png');
 }
 
 function setup() {
@@ -32,7 +32,8 @@ function setup() {
   playerX = width / 2 - playerSize / 2;
   playerY = height - playerSize - 100;
 
-  startPlatform = new Platform(width / 2 - 50, height - 100, 100, 20, 'rect');
+  platforms.push(new Platform(width / 2 - 50, height - 100, 100, 20, 'rect'));
+  coin = createCoin();
 }
 
 function draw() {
@@ -53,78 +54,104 @@ function drawStartMenu() {
 }
 
 function drawGame() {
-  tint(255, 128);
-  image(video, 0, 0, width, height);
-  noTint();
+  if (gameState === 'play') {
+    updatePlatforms();
+    updateCoin();
 
-  updatePlatforms();
+    let newPlayerX = playerX;
+    if (keyIsDown(LEFT_ARROW)) {
+      newPlayerX -= playerSpeed;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      newPlayerX += playerSpeed;
+    }
 
-  let newPlayerX = playerX;
-  if (keyIsDown(LEFT_ARROW)) {
-    newPlayerX -= playerSpeed;
-  }
-  if (keyIsDown(RIGHT_ARROW)) {
-    newPlayerX += playerSpeed;
-  }
+    let onPlatform = true;
 
-  let onPlatform = playerY + playerSize >= startPlatform.y &&
-    playerX + playerSize > startPlatform.x &&
-    playerX < startPlatform.x + startPlatform.width;
 
-  for (let platform of platforms) {
-    if (
-      newPlayerX < platform.x + platform.width &&
-      newPlayerX + playerSize > platform.x &&
-      playerY < platform.y + platform.height &&
-      playerY + playerSize > platform.y
-    ) {
-      onPlatform = true;
-      break;
+    playerX = newPlayerX;
+
+    // Handle jumping
+    if (keyIsDown(UP_ARROW) && onPlatform) {
+      playerVelocity = jumpForce;
+      if (!firstJump) {
+        firstJump = true; // Set firstJump to true when the player makes the first jump
+      }
+      console.log("Jump initiated. playerVelocity:", playerVelocity);
+    }
+
+    playerY += playerVelocity;
+    playerVelocity += gravity;
+
+    console.log("playerY:", playerY, "playerVelocity:", playerVelocity);
+
+    for (let platform of platforms) {
+      if (platform.collidesWith(playerX, playerY, playerSize)) {
+        playerY = platform.y - playerSize;
+        playerVelocity = 0;
+        onPlatform = true;
+        console.log("Collision detected. playerY adjusted:", playerY);
+        break;
+      }
+    }
+
+    console.log("onPlatform:", onPlatform);
+
+    if (firstJump) {
+      lavaY -= scrollSpeed;
+
+      // Update platforms and coin positions
+      platforms.forEach(platform => {
+        platform.y -= scrollSpeed;
+      });
+
+      coin.y -= scrollSpeed;
+    }
+
+    // Draw the mirrored video
+    push();
+    translate(width, 0);
+    scale(-1, 1);
+    image(video, 0, 0, width, height);
+    pop();
+
+    // Draw platforms and coin
+    platforms.forEach(platform => {
+      platform.draw();
+    });
+    image(coinImage, coin.x, coin.y, coinSize, coinSize);
+
+    fill(255, 0, 0);
+    noStroke();
+    rect(0, lavaY, width, height - lavaY);
+
+    if (playerY + playerSize > lavaY || playerY > height) {
+      gameState = 'start';
+      lavaY = height;
+      score = 0;
+      firstJump = false; // Reset firstJump when the game restarts
+      resetPlayer(); // Reset the player's position
+      resetPlatforms(); // Reset the platforms' positions
+      resetCoin(); // Reset the coin's position
+    }
+
+    if (playerY < height / 2) {
+      let offset = height / 2 - playerY;
+      playerY += offset;
+      lavaY += offset;
+      platforms.forEach(platform => platform.y += offset);
+      coin.y += offset;
+    }
+
+    // Remove off-screen platforms and coin
+    platforms = platforms.filter(platform => platform.y < height);
+    if (coin.y > height) {
+      coin = createCoin();
     }
   }
 
-  if (onPlatform) {
-    playerX = newPlayerX;
-    playerVelocity = 0;
-  } else {
-    playerVelocity += gravity;
-  }
-
-  // Handle jumping
-  if (keyIsDown(UP_ARROW) && onPlatform) {
-    playerVelocity = jumpForce;
-  }
-
-  playerY += playerVelocity;
-
-  lavaY -= scrollSpeed;
-
-  fill(255, 0, 0);
-  noStroke();
-  rect(0, lavaY, width, height - lavaY);
-
-  if (playerY + playerSize > lavaY || playerY > height) {
-    gameState = 'start';
-    lavaY = height;
-    score = 0;
-    playerY = startPlatform.y - playerSize;
-    playerX = startPlatform.x + startPlatform.width / 2 - playerSize / 2;
-  }
-
-  if (playerY < height / 2) {
-    let offset = height / 2 - playerY;
-    playerY += offset;
-    lavaY += offset;
-    platforms.forEach(platform => platform.y += offset);
-    startPlatform.y += offset;
-  }
-
-  platforms.forEach(platform => platform.draw());
-  startPlatform.draw();
-
   // Draw player
   image(player, playerX, playerY - playerSize, playerSize, playerSize * 2.37);
-  image(coin, width / 2 - coinSize / 2, 50, coinSize, coinSize);
 
   score = Math.max(score, height - playerY);
 
@@ -132,9 +159,22 @@ function drawGame() {
   textSize(24);
   text('Score: ' + score, 20, 40);
 }
+function resetPlayer() {
+  playerY = height - playerSize - 100;
+  playerX = width / 2 - playerSize / 2;
+  playerVelocity = 0;
+} function resetPlatforms() {
+  platforms.forEach(platform => {
+    platform.y = height - 100; // Reset the platforms' y-position
+  });
+}
 
+function resetCoin() {
+  coin.y = height - coinSize - 100; // Reset the coin's y-position
+}
 function updatePlatforms() {
-  platforms = [];
+  platforms = platforms.filter(platform => platform.y < height);
+
   video.loadPixels();
   for (let y = 0; y < video.height; y += 20) {
     for (let x = 0; x < video.width; x += 20) {
@@ -143,7 +183,7 @@ function updatePlatforms() {
       let g = video.pixels[index + 1];
       let b = video.pixels[index + 2];
 
-      if (r > 150 && g < 100 && b < 100) {
+      if (r > 200 && g > 200 && b > 200) {
         let choice = floor(random(3));
         if (choice === 0) {
           platforms.push(new Platform(x, y, 20, 20, 'ellipse'));
@@ -155,6 +195,20 @@ function updatePlatforms() {
       }
     }
   }
+}
+
+function updateCoin() {
+  if (dist(playerX + playerSize / 2, playerY + playerSize / 2, coin.x + coinSize / 2, coin.y + coinSize / 2) < (playerSize + coinSize) / 2) {
+    coin = createCoin();
+    score++;
+  }
+}
+
+function createCoin() {
+  return {
+    x: random(width - coinSize),
+    y: random(height - coinSize)
+  };
 }
 
 function mousePressed() {
@@ -173,7 +227,7 @@ class Platform {
   }
 
   draw() {
-    fill(255, 0, 0);
+    fill(0, 255, 255);
     noStroke();
     if (this.shape === 'ellipse') {
       ellipse(this.x + this.width / 2, this.y + this.height / 2, this.width, this.height);
@@ -189,5 +243,14 @@ class Platform {
         this.y
       );
     }
+  }
+
+  collidesWith(playerX, playerY, playerSize) {
+    return (
+      playerX < this.x + this.width &&
+      playerX + playerSize > this.x &&
+      playerY + playerSize > this.y &&
+      playerY + playerSize < this.y + this.height
+    );
   }
 }
