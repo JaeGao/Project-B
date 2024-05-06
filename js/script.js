7acb74e3f13f18bd937fdb40b64612f23866b63c
JaeGao <jg6523@nyu.edu>
2024-05-07 06:06:47 +0800

    game done

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let player;
 let coin;
 let platforms = [];
 let score = 0;
+let coinCount = 0;
 let gameState = 'start';
 let playerX;
 let playerY;
@@ -17,9 +18,24 @@ const gravity = 0.6;
 let playerWidth = 154;
 let playerHeight = 365;
 let firstJump = false;
+let startTime;
+let gameTime = 0;
+let gameTimer = 60000; // 1 minute in milliseconds
+let jumpSound;
+let collectSound;
+let gameMusic;
+let loseSound;
+let winSound;
+let isCrouching = false;
+
 function preload() {
   player = loadImage('assets/player.png');
   coinImage = loadImage('assets/coin.png');
+  jumpSound = loadSound('assets/jump.mp3');
+  collectSound = loadSound('assets/collect.mp3');
+  gameMusic = loadSound('assets/game-music-loop.mp3');
+  loseSound = loadSound('assets/violin-lose.mp3');
+  winSound = loadSound('assets/game-win.mp3');
 }
 
 function setup() {
@@ -41,16 +57,22 @@ function draw() {
     drawStartMenu();
   } else if (gameState === 'play') {
     drawGame();
+  } else if (gameState === 'end') {
+    drawEndScreen();
   }
 }
 
 function drawStartMenu() {
-  background(255);
+  background('#fff0f5');
   textAlign(CENTER);
-  textSize(32);
-  text('Two-Player Platformer', width / 2, height / 2 - 50);
+  textSize(48);
+  textFont('Press Start 2P');
+  fill('#d64d4d');
+  text('Two-Player Platformer', width / 2, height / 2 - 100);
   textSize(24);
-  text('Click to Start', width / 2, height / 2);
+  textFont('Courier New');
+  fill('#333');
+  text('Press Enter to Start', width / 2, height / 2);
 }
 
 function drawGame() {
@@ -59,43 +81,60 @@ function drawGame() {
     updateCoin();
 
     let newPlayerX = playerX;
-    if (keyIsDown(LEFT_ARROW)) {
+    if (keyIsDown(65)) { // A key
       newPlayerX -= playerSpeed;
     }
-    if (keyIsDown(RIGHT_ARROW)) {
+    if (keyIsDown(68)) { // D key
       newPlayerX += playerSpeed;
     }
 
-    let onPlatform = true;
-
+    let onPlatform = false;
+    for (let platform of platforms) {
+      if (platform.collidesWith(newPlayerX, playerY, playerSize)) {
+        onPlatform = true;
+        break;
+      }
+    }
 
     playerX = newPlayerX;
-
-    // Handle jumping
-    if (keyIsDown(UP_ARROW) && onPlatform) {
-      playerVelocity = jumpForce;
-      if (!firstJump) {
-        firstJump = true; // Set firstJump to true when the player makes the first jump
-      }
-      console.log("Jump initiated. playerVelocity:", playerVelocity);
-    }
 
     playerY += playerVelocity;
     playerVelocity += gravity;
 
-    console.log("playerY:", playerY, "playerVelocity:", playerVelocity);
-
+    onPlatform = false; // Reset onPlatform before collision detection
     for (let platform of platforms) {
       if (platform.collidesWith(playerX, playerY, playerSize)) {
         playerY = platform.y - playerSize;
         playerVelocity = 0;
         onPlatform = true;
-        console.log("Collision detected. playerY adjusted:", playerY);
         break;
       }
     }
 
-    console.log("onPlatform:", onPlatform);
+    // Handle jumping
+    if (keyIsDown(87) && onPlatform) { // W key
+      playerVelocity = jumpForce;
+      jumpSound.play();
+      if (!firstJump) {
+        firstJump = true; // Set firstJump to true when the player makes the first jump
+      }
+    }
+
+    // Handle crouching
+    if (keyIsDown(83)) { // S key
+      isCrouching = true;
+    } else {
+      isCrouching = false;
+    }
+
+    // Check collision with the coin
+    if (dist(playerX + playerSize / 2, playerY + playerSize / 2, coin.x + coinSize / 2, coin.y + coinSize / 2) < (playerSize + coinSize) / 2) {
+      coin = createCoin();
+      coinCount++;
+      score += 100; // Increase the score by 100 points for collecting a coin
+      flashMessage("COIN COLLECTED!", 60);
+      collectSound.play();
+    }
 
     if (firstJump) {
       lavaY -= scrollSpeed;
@@ -121,18 +160,18 @@ function drawGame() {
     });
     image(coinImage, coin.x, coin.y, coinSize, coinSize);
 
-    fill(255, 0, 0);
+    fill(255, 100, 0);
     noStroke();
     rect(0, lavaY, width, height - lavaY);
 
     if (playerY + playerSize > lavaY || playerY > height) {
-      gameState = 'start';
-      lavaY = height;
-      score = 0;
-      firstJump = false; // Reset firstJump when the game restarts
-      resetPlayer(); // Reset the player's position
-      resetPlatforms(); // Reset the platforms' positions
-      resetCoin(); // Reset the coin's position
+      gameState = 'end';
+      gameMusic.stop();
+      loseSound.play();
+    } else if (gameTime >= gameTimer) {
+      gameState = 'end';
+      gameMusic.stop();
+      winSound.play();
     }
 
     if (playerY < height / 2) {
@@ -151,27 +190,58 @@ function drawGame() {
   }
 
   // Draw player
-  image(player, playerX, playerY - playerSize, playerSize, playerSize * 2.37);
+  if (isCrouching) {
+    image(player, playerX, playerY - playerSize + (playerSize * 1.37), playerSize, playerSize);
+  } else {
+    image(player, playerX, playerY - playerSize, playerSize, playerSize * 2.37);
+  }
 
-  score = Math.max(score, height - playerY);
+  updateGameTime();
 
   textAlign(LEFT);
   textSize(24);
+  textFont('Roboto Mono');
+  fill('#3a4660');
   text('Score: ' + score, 20, 40);
+  text('Coins: ' + coinCount, 20, 70);
+  text('Time: ' + formatTime(gameTime), 20, 100);
 }
+
+function drawEndScreen() {
+  background('#fff0f5');
+  textAlign(CENTER);
+  textSize(48);
+  textFont('Press Start 2P');
+  fill('#d64d4d');
+  if (gameTime >= gameTimer) {
+    text('Congratulations!', width / 2, height / 2 - 100);
+  } else {
+    text('Game Over!', width / 2, height / 2 - 100);
+  }
+  textSize(24);
+  textFont('Courier New');
+  fill('#333');
+  text('Final Score: ' + score, width / 2, height / 2);
+  text('Coins Collected: ' + coinCount, width / 2, height / 2 + 40);
+  text('Time Survived: ' + formatTime(gameTime), width / 2, height / 2 + 80);
+  text('Press Enter to Restart', width / 2, height / 2 + 120);
+}
+
 function resetPlayer() {
   playerY = height - playerSize - 100;
   playerX = width / 2 - playerSize / 2;
   playerVelocity = 0;
-} function resetPlatforms() {
-  platforms.forEach(platform => {
-    platform.y = height - 100; // Reset the platforms' y-position
-  });
+}
+
+function resetPlatforms() {
+  platforms = [];
+  platforms.push(new Platform(width / 2 - 50, height - 100, 100, 20, 'rect'));
 }
 
 function resetCoin() {
-  coin.y = height - coinSize - 100; // Reset the coin's y-position
+  coin = createCoin();
 }
+
 function updatePlatforms() {
   platforms = platforms.filter(platform => platform.y < height);
 
@@ -183,7 +253,7 @@ function updatePlatforms() {
       let g = video.pixels[index + 1];
       let b = video.pixels[index + 2];
 
-      if (r > 200 && g > 200 && b > 200) {
+      if (r > 250 && g > 250 && b > 250) {
         let choice = floor(random(3));
         if (choice === 0) {
           platforms.push(new Platform(x, y, 20, 20, 'ellipse'));
@@ -200,7 +270,10 @@ function updatePlatforms() {
 function updateCoin() {
   if (dist(playerX + playerSize / 2, playerY + playerSize / 2, coin.x + coinSize / 2, coin.y + coinSize / 2) < (playerSize + coinSize) / 2) {
     coin = createCoin();
-    score++;
+    coinCount++;
+    score += 100; // Increase the score by 100 points for collecting a coin
+    flashMessage("COIN COLLECTED!", 60);
+    collectSound.play();
   }
 }
 
@@ -211,10 +284,26 @@ function createCoin() {
   };
 }
 
-function mousePressed() {
-  if (gameState === 'start') {
+function keyPressed() {
+  if (gameState === 'start' && keyCode === ENTER) {
     gameState = 'play';
+    startTime = millis(); // Record the start time when the game begins
+    gameMusic.loop();
+  } else if (gameState === 'end' && keyCode === ENTER) {
+    resetGame();
   }
+}
+
+function resetGame() {
+  gameState = 'start';
+  lavaY = height;
+  score = 0;
+  coinCount = 0;
+  firstJump = false;
+  resetPlayer();
+  resetPlatforms();
+  resetCoin();
+  gameTime = 0;
 }
 
 class Platform {
@@ -230,16 +319,16 @@ class Platform {
     fill(0, 255, 255);
     noStroke();
     if (this.shape === 'ellipse') {
-      ellipse(this.x + this.width / 2, this.y + this.height / 2, this.width, this.height);
+      ellipse(width - this.x - this.width / 2, this.y + this.height / 2, this.width, this.height);
     } else if (this.shape === 'rect') {
-      rect(this.x, this.y, this.width, this.height);
+      rect(width - this.x - this.width, this.y, this.width, this.height);
     } else if (this.shape === 'triangle') {
       triangle(
-        this.x,
+        width - this.x - this.width,
         this.y + this.height,
-        this.x + this.width,
+        width - this.x,
         this.y + this.height,
-        this.x + this.width / 2,
+        width - this.x - this.width / 2,
         this.y
       );
     }
@@ -247,10 +336,30 @@ class Platform {
 
   collidesWith(playerX, playerY, playerSize) {
     return (
-      playerX < this.x + this.width &&
-      playerX + playerSize > this.x &&
+      playerX < width - this.x &&
+      playerX + playerSize > width - this.x - this.width &&
       playerY + playerSize > this.y &&
       playerY + playerSize < this.y + this.height
     );
   }
+}
+
+function flashMessage(message, size) {
+  textAlign(CENTER);
+  textSize(size);
+  fill(255, 255, 0);
+  text(message, width / 2, height / 2);
+}
+
+function updateGameTime() {
+  if (gameState === 'play') {
+    gameTime = millis() - startTime;
+  }
+}
+
+function formatTime(time) {
+  let minutes = floor(time / 60000);
+  let seconds = floor((time % 60000) / 1000);
+  let milliseconds = floor((time % 1000) / 100);
+  return nf(minutes, 2) + ":" + nf(seconds, 2) + "." + nf(milliseconds, 1);
 }
